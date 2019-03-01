debe52d7c834485856c6db7ae9b645194af259f5
ShayRubach <shayrubach@gmail.com>
2019-03-01 05:07:19 +0200

    introduced ExpBasicData class

---
+++ app/src/main/java/com/ezaf/www/citisci/data/ExpBasicData.kt
@@ -0,0 +1,15 @@
+package com.ezaf.www.citisci.data
+
+import java.util.*
+
+internal class ExpBasicData (
+        private val id: String,
+        private val name: String,
+        private val startTime: Date,
+        private val researcher: String,
+        private val automatic: Boolean,
+        private val desc: String,
+        private val guide: String)
+{
+    
+}
