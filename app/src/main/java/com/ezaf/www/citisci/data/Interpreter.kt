657ff34e6f3dc26341c728054474d40788af7873
ShayRubach <shayrubach@gmail.com>
2019-03-01 05:55:18 +0200

    introduced the Interpreter singleton object

---
+++ app/src/main/java/com/ezaf/www/citisci/data/Interpreter.kt
@@ -0,0 +1,12 @@
+package com.ezaf.www.citisci.data
+
+object Interpreter {
+
+    fun playScripts(expId: String) {
+        //TODO: playScripts() implemeted this - what happens on multiple experiments? does this thread hold the lock?
+    }
+
+    fun stopScripts(expId: String) {
+        //TODO: playScripts() implemeted this - what happens on multiple experiments? does this thread hold the lock?
+    }
+}
