1ef3830d525a95625b72060f0ea2c1d03182a4cc
ShayRubach <shayrubach@gmail.com>
2019-03-01 05:32:34 +0200

    introduced GpsExpCondition class

---
+++ app/src/main/java/com/ezaf/www/citisci/data/GpsExpCondition.kt
@@ -0,0 +1,17 @@
+package com.ezaf.www.citisci.data
+
+class GpsExpCondition(val baseCoord: Pair<Double,Double>,
+                      val maxRadius: Double,
+                      _id: String,
+                      _sensorType: SensorType) : ExpCondition
+{
+    override val id = _id
+//        get() = super.id
+
+    override val sensorType = _sensorType
+//        get() = super.sensorType
+
+    override fun isConditionMet(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
