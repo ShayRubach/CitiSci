c9a8daa87db44ef26b3844e249736b5182cc1c9c
ShayRubach <shayrubach@gmail.com>
2019-03-01 05:54:55 +0200

    introduced DatabaseHandler class

---
+++ app/src/main/java/com/ezaf/www/citisci/data/DatabaseHandler.kt
@@ -0,0 +1,13 @@
+package com.ezaf.www.citisci.data
+
+class DatabaseHandler(val url: String,
+                      val dbName: String)
+{
+    fun connect() {
+        //TODO: connect() implement this
+    }
+
+    fun sendMsg() {
+        //TODO: sendMsg() implement this
+    }
+}
