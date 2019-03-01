8b02f3c6e8f827326533f285361d31a3e5e3465a
ShayRubach <shayrubach@gmail.com>
2019-03-01 05:21:19 +0200

    remove redundant access modifiers

--- app/src/main/java/com/ezaf/www/citisci/data/ExpBasicData.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/ExpBasicData.kt
@@ -3,13 +3,13 @@ package com.ezaf.www.citisci.data
 import java.util.*
 
 internal class ExpBasicData (
-        private val id: String,
-        private val name: String,
-        private val startTime: Date,
-        private val researcher: String,
-        private val automatic: Boolean,
-        private val desc: String,
-        private val guide: String)
+        val id: String,
+        val name: String,
+        val startTime: Date,
+        val researcher: String,
+        val automatic: Boolean,
+        val desc: String,
+        val guide: String)
 {
 
 }
