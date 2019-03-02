3e7049d74038a37766e5342243fc1f55e7dec8b9
ShayRubach <shayrubach@gmail.com>
2019-03-02 03:48:58 +0200

    added a data binding test button. modified gradle.

--- app/src/main/java/com/ezaf/www/citisci/MainActivity.kt
+++ app/src/main/java/com/ezaf/www/citisci/MainActivity.kt
@@ -2,6 +2,12 @@ package com.ezaf.www.citisci
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ezaf.www.citisci.databinding.ActivityMainBinding
+import com.ezaf.www.citisci.utils.MainViewModel
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.Observer
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainViewModel = ViewModelProviders.of(this)
+                .get(MainViewModel::class.java)
+
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+                this, R.layout.activity_main
+        ).apply {
+            this.setLifecycleOwner(this@MainActivity)
+            this.viewmodel = mainViewModel
+        }
+
+//        mainViewModel.editTextContent.observe(this, Observer {
+//            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+//        })
 
     }
 }
--- app/src/main/java/com/ezaf/www/citisci/utils/MainViewModel.kt
+++ app/src/main/java/com/ezaf/www/citisci/utils/MainViewModel.kt
@@ -1,9 +1,15 @@
 package com.ezaf.www.citisci.utils
 
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.Bindable
 import androidx.lifecycle.ViewModel
+import com.ezaf.www.citisci.MainActivity
 
 class MainViewModel : ViewModel() {
 
-
+    fun onAckTest(){
+        Log.d("testDbg", "ack");
+    }
 
 }
