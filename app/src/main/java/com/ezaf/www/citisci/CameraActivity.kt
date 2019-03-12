d973bd24fc6c5505a9c8ff89dc32aff9fea9fa52
ShayRubach <shayrubach@gmail.com>
2019-03-12 07:59:22 +0200

    introduced CameraActivity for taking and locally saving photos

---
+++ app/src/main/java/com/ezaf/www/citisci/CameraActivity.kt
@@ -0,0 +1,125 @@
+package com.ezaf.www.citisci
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.ezaf.www.citisci.utils.Logger
+import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CameraActivity : AppCompatActivity() {
+
+    /*
+    val CAMERA_REQUEST_CODE = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        //TODO: change to view bind using a CameraViewModel
+        takePictureBtn.setOnClickListener{
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(callCameraIntent.resolveActivity(packageManager) != null){
+                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+
+            }
+        }
+    }
+
+    //TODO: move logic to CameraViewModel
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+
+            CAMERA_REQUEST_CODE ->{
+                if(resultCode == Activity.RESULT_OK && data != null){
+                    photoImgView.setImageBitmap(data.extras.get("data") as Bitmap)
+                }
+            }
+
+
+            else -> {
+                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+    */
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_TAKE_PHOTO= 2
+    private var currentPhotoPath: String = "."
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        //TODO: change to view bind using a CameraViewModel
+        takePictureBtn.setOnClickListener{
+            dispatchTakePictureIntent()
+        }
+    }
+
+    //TODO: move logic to CameraViewModel
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+                "JPEG_${timeStamp}_", /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    //TODO: move logic to CameraViewModel
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    Logger.log("dbg","error creating image file")
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                            this,
+                            getString(R.string.fileprovider_path),
+                            it
+                    )
+                    Logger.log("dbg","successfully got a file from FileProvider")
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+    //TODO: move logic to CameraViewModel
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            photoImgView.setImageBitmap(imageBitmap)
+        }
+    }
+}
