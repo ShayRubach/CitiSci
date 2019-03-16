352a985e93ce8477a41b572716942ea3f66dea3e
ShayRubach <shayrubach@gmail.com>
2019-03-16 05:31:56 +0200

    SensorType: added Uknown type for default return value. added toSensorType() imp

--- app/src/main/java/com/ezaf/www/citisci/data/SensorType.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/SensorType.kt
@@ -3,5 +3,15 @@ package com.ezaf.www.citisci.data
 enum class SensorType {
     GPS,
     Michrophone,
-    Camera
+    Camera,
+    Unknown
+}
+
+fun toSensorType(str:String): SensorType {
+    return when(str){
+        SensorType.GPS.toString() -> SensorType.GPS
+        SensorType.Michrophone.toString() -> SensorType.Michrophone
+        SensorType.Camera.toString() -> SensorType.Camera
+        else -> SensorType.Unknown
+    }
 }
