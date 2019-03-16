c766247d1b9b81ec3767f55ec211009f2d03a636
ShayRubach <shayrubach@gmail.com>
2019-03-16 05:30:26 +0200

    ExpScript: added toString() imp

--- app/src/main/java/com/ezaf/www/citisci/data/ExpScript.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/ExpScript.kt
@@ -3,5 +3,7 @@ package com.ezaf.www.citisci.data
 class ExpScript(val expActions: MutableList<ExpAction>,
                 val expConditions: MutableList<ExpCondition>)
 {
-
+    override fun toString(): String {
+        return "$expActions\n$expConditions"
+    }
 }
