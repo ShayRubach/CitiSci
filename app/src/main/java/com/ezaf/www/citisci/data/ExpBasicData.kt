793c76286e50adb0c38086d91285711b455c26a3
ShayRubach <shayrubach@gmail.com>
2019-03-16 05:29:46 +0200

    ExpBasicData: replaced Date with Instant type. added toString() imp

--- app/src/main/java/com/ezaf/www/citisci/data/ExpBasicData.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/ExpBasicData.kt
@@ -1,15 +1,24 @@
 package com.ezaf.www.citisci.data
 
-import java.util.*
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class ExpBasicData (
         val id: String,
         val name: String,
-        val startTime: Date,
+        val startTime: Instant,
         val researcher: String,
         val automatic: Boolean,
         val desc: String,
         val guide: String)
 {
-
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun toString(): String {
+        return "$id|$name|"+
+                DateTimeFormatter.ofPattern("yyyy-MM-dd@HH:mm:ss.SSSSSS").withZone( ZoneId.systemDefault() ).format(startTime) +
+                "|$researcher|$automatic|$desc|$guide"
+    }
 }
