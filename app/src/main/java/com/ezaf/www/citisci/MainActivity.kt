fa13917efe370ac9196a9ff00f1b26200ed7d94a
ShayRubach <shayrubach@gmail.com>
2019-03-18 05:27:13 +0200

    added a db test logic - will be removed later on

--- app/src/main/java/com/ezaf/www/citisci/MainActivity.kt
+++ app/src/main/java/com/ezaf/www/citisci/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.ezaf.www.citisci
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.ezaf.www.citisci.databinding.ActivityMainBinding
 import com.ezaf.www.citisci.utils.MainViewModel
 import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.Observer
-import android.widget.Toast
+import com.ezaf.www.citisci.data.*
+import com.ezaf.www.citisci.utils.Logger.log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Instant
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var db: LocalDatabase? = null
+    }
+    var expDao: ExperimentDao? = null
 
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,12 +32,12 @@ class MainActivity : AppCompatActivity() {
         val mainViewModel = ViewModelProviders.of(this)
                 .get(MainViewModel::class.java)
 
-        DataBindingUtil.setContentView<ActivityMainBinding>(
-                this, R.layout.activity_main
-        ).apply {
-            this.setLifecycleOwner(this@MainActivity)
-            this.viewmodel = mainViewModel
-        }
+//        DataBindingUtil.setContentView<ActivityMainBinding>(
+//                this, R.layout.activity_main
+//        ).apply {
+//            this.setLifecycleOwner(this@MainActivity)
+//            this.viewmodel = mainViewModel
+//        }
 
         goToCameraBtn.setOnClickListener{
             startActivity(Intent(this, CameraActivity::class.java))
@@ -38,11 +49,43 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
+        db = LocalDatabase.getLocalDb(context = this)
+        expDao = db?.experimentDao()
+        testDbInsertionAndSelection()
 
 
-//        mainViewModel.editTextContent.observe(this, Observer {
-//            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-//        })
+    }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun testDbInsertionAndSelection(){
+        Observable.fromCallable {
+
+            var action = ExpAction(2.4,2,100,SensorType.GPS)
+            var cond = GpsExpCondition(Pair(100.0,100.0),500.0,"1234",SensorType.GPS)
+            var bdata = ExpBasicData("123","name", Instant.now(),"researcher",false,"desc","guide")
+            var exp = Experiment(123,ExpScript(expActions = mutableListOf(action),expConditions = mutableListOf(cond)),bdata)
+
+            var action2 = ExpAction(2.4,2,100,SensorType.GPS)
+            var cond2 = GpsExpCondition(Pair(100.0,100.0),500.0,"1234",SensorType.GPS)
+            var bdata2 = ExpBasicData("345","name", Instant.now(),"researcher",false,"desc","guide")
+            var exp2 = Experiment(678,ExpScript(expActions = mutableListOf(action2),expConditions = mutableListOf(cond2)),bdata2)
+
+//            db?.clearAllTables()
+
+            with(expDao){
+//                this?.insertExp(exp)
+//                this?.insertExp(exp2)
+
+            }
+            db?.experimentDao()?.getAllExp()!!
+        }.doOnNext {
+            for(x:Experiment in it) {
+                var re = Regex("\\[|\\]")
+                var str = re.replace(x.toString(),"")
+                log("dbg", " \n##################\n$str\n##################\n")
+            }
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
     }
 }
