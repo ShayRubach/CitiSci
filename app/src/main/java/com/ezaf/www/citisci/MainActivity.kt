a73cb9e85aa789f37ba7b622d7b9adc15cc9bee7
ShayRubach <shayrubach@gmail.com>
2019-03-19 03:01:51 +0200

    MainActivity: removed redundant code

--- app/src/main/java/com/ezaf/www/citisci/MainActivity.kt
+++ app/src/main/java/com/ezaf/www/citisci/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.ezaf.www.citisci
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.ezaf.www.citisci.utils.MainViewModel
 import androidx.lifecycle.ViewModelProviders
@@ -24,7 +22,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -56,7 +53,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun testDbInsertionAndSelection(){
         Observable.fromCallable {
 
