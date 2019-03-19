6a4969cc740d8d11e7ddddec11791b9a372e2899
ShayRubach <shayrubach@gmail.com>
2019-03-19 03:02:58 +0200

    introduced DataCollecter object to invoke the collection methods by sensor type

---
+++ app/src/main/java/com/ezaf/www/citisci/data/DataCollecter.kt
@@ -0,0 +1,20 @@
+package com.ezaf.www.citisci.data
+
+import org.json.JSONObject
+import java.time.Instant
+
+object DataCollecter {
+
+    fun collect(sensorType: SensorType, startTime: Instant) : JSONObject? {
+        return when(sensorType){
+            SensorType.GPS-> collectGpsCoord()
+            SensorType.Camera-> null
+            SensorType.Michrophone-> null
+            SensorType.Unknown -> null
+        }
+    }
+
+    private fun collectGpsCoord(): JSONObject? {
+        TODO("not implemented")
+    }
+}
