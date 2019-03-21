8df905deacb95e575f67bba5346e40f991cd3d0e
ShayRubach <shayrubach@gmail.com>
2019-03-21 04:14:15 +0200

    Interpreter: added a thread safe task list to add new tasks for the ScriptRunner to take. sanity check with single experiment is presented

--- app/src/main/java/com/ezaf/www/citisci/data/Interpreter.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/Interpreter.kt
@@ -1,53 +1,59 @@
 package com.ezaf.www.citisci.data
 
 import com.ezaf.www.citisci.MainActivity
+import com.ezaf.www.citisci.utils.Logger.log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import java.time.Instant
+import com.ezaf.www.citisci.utils.VerboseLevel.*
 
 object Interpreter {
 
+    var tasks = mutableListOf<ScriptRunner>()
+
     fun playScripts(expId: String) {
         //TODO: playScripts() implemeted this - what happens on multiple experiments? does this thread hold the lock?
-        var exp = MainActivity.db?.experimentDao()?.getExpById(expId)
-        exp?.run {
-            var actionList = expScript.expActions
-            var condList = expScript.expConditions
+        var fn = Throwable().stackTrace[0].methodName
+        log(INFO, "$fn: called.")
 
-            for (action in actionList){
-                playScriptFor(action, condList, exp.expBasicData.startTime)
-            }
-        }
+        Observable.fromCallable {
+            MainActivity.db?.experimentDao()?.getExpById(expId)!!
+        }.doOnNext {
+            var re = Regex("\\[|\\]")
+            var str = re.replace(it.toString(),"")
+
+            var actionList = it.expScript.expActions
+            var condList = it.expScript.expConditions
+
+            log(INFO,str)
+
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
+
     }
 
     fun stopScripts(expId: String) {
         //TODO: playScripts() implemeted this - what happens on multiple experiments? does this thread hold the lock?
+        var fn = Throwable().stackTrace[0].methodName
+        log(INFO_ERR, "$fn: called.")
     }
 
-    private fun playScriptFor(action: ExpAction, condList: MutableList<ExpCondition>, startTime: Instant){
+    private fun playScriptFor(action: ExpAction, condList: MutableList<ExpCondition>, startTime: Instant) {
+        var fn = Throwable().stackTrace[0].methodName
+        log(INFO_ERR, "$fn: called.")
+
         when(action.sensorType){
-            SensorType.GPS-> playGpsScript(action, condList.filter { it is GpsExpCondition } as MutableList<GpsExpCondition>, startTime)
-            SensorType.Camera-> playCameraScript()
-            SensorType.Michrophone-> playMicScript()
+            SensorType.GPS-> {
+                //add new task to the list and run the script
+                tasks.add(ScriptRunner(action, condList.filter { it is GpsExpCondition } as MutableList<GpsExpCondition>, startTime))
+                tasks[tasks.lastIndex].run()
+            }
+            SensorType.Camera-> return TODO("implement CameraExpCondition and invoke playCameraScript() from ScripRunner")
+            SensorType.Michrophone-> return TODO("implement MicExpCondition and invoke playCameraScript() from ScripRunner")
             SensorType.Unknown -> return
         }
     }
 
-    private fun playGpsScript(action: ExpAction, conds: List<GpsExpCondition>, startTime: Instant) {
-
-        val condCheck: (GpsExpCondition) -> Boolean = { it.isConditionMet() }
-        if(conds.all(condCheck)){
-            action.run {
-                if(expDurationHasEnded(startTime) || allSamplesWereCollected()){
-                    //TODO: endExperiment(): implement
-                    //endExperiment()
-                }
-                else{
-                    collectData(startTime)
-                }
-            }
-        }
-
-    }
-
-    private fun playCameraScript(){}
-    private fun playMicScript(){}
 }
