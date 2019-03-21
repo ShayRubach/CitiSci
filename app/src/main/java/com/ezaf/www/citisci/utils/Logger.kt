7d00c6536ac683d7df3d835ef32b6dd8d75f38a1
ShayRubach <shayrubach@gmail.com>
2019-03-21 04:01:23 +0200

    Logger: added verbose lvl filtering to distinguish info/err dbgs or else.

--- app/src/main/java/com/ezaf/www/citisci/utils/Logger.kt
+++ app/src/main/java/com/ezaf/www/citisci/utils/Logger.kt
@@ -4,9 +4,19 @@ import android.util.Log
 object Logger {
 
     val ENABLE_DBG_PRINTS = true
+    val GLOBAL_VERBOSE_LVL = VerboseLevel.INFO_ERR    //change this to filter dbg lvls by tags
 
-    fun log(printGroup: String, msg: String) {
-        if(ENABLE_DBG_PRINTS)
-            Log.d(printGroup,msg)
+    fun log(lvl: VerboseLevel = VerboseLevel.INFO_ERR, msg: String) {
+        if(ENABLE_DBG_PRINTS && GLOBAL_VERBOSE_LVL >= lvl)
+            Log.d(lvl.toString(), msg)
     }
+}
+
+enum class VerboseLevel(lvl: Int) {
+    PLACEHOLDER_1(1),
+    PLACEHOLDER_2(2),
+    INFO(3),
+    ERR(4),
+    INFO_ERR(5)
+
 }
