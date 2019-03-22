3e7ff7186718b23c42f4f4d6cf67a1d86e44332d
ShayRubach <shayrubach@gmail.com>
2019-03-22 04:34:48 +0200

    ScriptRunner: added interval observable to call the data acquisition process according to the capture interval.

--- app/src/main/java/com/ezaf/www/citisci/data/ScriptRunner.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/ScriptRunner.kt
@@ -2,20 +2,36 @@ package com.ezaf.www.citisci.data
 
 import com.ezaf.www.citisci.utils.Logger.log
 import com.ezaf.www.citisci.utils.VerboseLevel.*
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import java.time.Instant
+import java.util.concurrent.TimeUnit
 
 class ScriptRunner(
-        val action: ExpAction,
-        val conds: List<GpsExpCondition>,
-        val startTime: Instant
-) : Runnable {
+        private val action: ExpAction,
+        private val conds: List<GpsExpCondition>,
+        private val startTime: Instant
+) {
 
-    override fun run() {
+    fun collect() {
         val fn = object{}.javaClass.enclosingMethod.name
         log(INFO,"$fn: called. [sensorType = ${action.sensorType}]")
 
         when(action.sensorType){
-            SensorType.GPS-> playGpsScript(action,conds,startTime)
+            SensorType.GPS-> {
+
+                Interpreter.observablesManager.add(
+                        Observable.interval(action.captureInterval.toLong(), TimeUnit.SECONDS)
+                                .timeInterval()
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe{
+                                    playGpsScript(action, conds, startTime)
+                                }
+                )
+
+            }
             SensorType.Camera-> return
             SensorType.Michrophone-> return
             SensorType.Unknown -> return
@@ -28,15 +44,21 @@ class ScriptRunner(
 
         val condCheck: (GpsExpCondition) -> Boolean = { it.isConditionMet() }
         if(conds.all(condCheck)){
+            log(INFO,"$fn: conditions met.")
             action.run {
                 if(expDurationHasEnded(startTime) || allSamplesWereCollected()){
+                    log(INFO,"$fn: experiment duration has ended or all samples were collected. ending experiment")
                     //TODO: endExperiment(): implement
                     //endExperiment()
                 }
                 else{
+                    log(INFO,"$fn: calling data collector")
                     //collectData()
                 }
             }
+        }
+        else{
+            log(INFO,"$fn: conditions did not met. retrying again in ${action.captureInterval} seconds")
         }
 
     }
