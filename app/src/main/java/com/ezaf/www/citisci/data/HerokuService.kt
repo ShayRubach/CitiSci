b8672bca6b4abd501746f7dfed9376ab7ef88db5
ShayRubach <shayrubach@gmail.com>
2019-03-23 08:01:08 +0200

    HerokuService: added additional GET POST PUT operations

--- app/src/main/java/com/ezaf/www/citisci/data/HerokuService.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/HerokuService.kt
@@ -1,10 +1,35 @@
 package com.ezaf.www.citisci.data
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.*
 
 interface HerokuService {
     @GET("https://buxa.herokuapp.com/")
     fun getMyData(): Call<ResponseBody>
+
+    @GET("https://tempcitisci.herokuapp.com/experiment/all")
+    fun getAllExperiments(): Call<ResponseBody>
+
+    @GET("https://tempcitisci.herokuapp.com/experiment/findByID/:{id}")
+    fun getExpById(@Path("id")id: String): Call<ResponseBody>
+
+    @POST("https://tempcitisci.herokuapp.com/experiment/create")
+    fun createDummyExp(): Call<ResponseBody>
+
+    @PUT("https://tempcitisci.herokuapp.com/experiment/addSamples/:{id}")
+    fun putSample(@Path("id")id: String): Call<ResponseBody>
+
+//    In the body of the msg send a json like:
+//    {
+//
+//        "samples":
+//        {
+//
+//            "x":45,
+//
+//            "y":15
+//
+//        }
+//
+//    }
 }
