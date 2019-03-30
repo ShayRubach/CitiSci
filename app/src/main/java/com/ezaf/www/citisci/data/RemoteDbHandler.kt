af0886d8f4dc6a12dc94e0b2e1cdf731a9ba2359
ShayRubach <shayrubach@gmail.com>
2019-03-30 09:36:16 +0200

    RemoteDbHandler: added getAllExp() wrapper fun

--- app/src/main/java/com/ezaf/www/citisci/data/RemoteDbHandler.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/RemoteDbHandler.kt
@@ -1,5 +1,6 @@
 package com.ezaf.www.citisci.data
 
+import com.ezaf.www.citisci.MainActivity
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
@@ -10,6 +11,11 @@ import retrofit2.Response
 import com.ezaf.www.citisci.data.RemoteDbHandler.MsgType.*
 import com.ezaf.www.citisci.utils.Logger
 import com.ezaf.www.citisci.utils.VerboseLevel
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import okhttp3.MediaType
 import okhttp3.RequestBody
 import retrofit2.converter.gson.GsonConverterFactory
@@ -44,11 +50,12 @@ object RemoteDbHandler
         Logger.log(VerboseLevel.INFO, "$fn: called.")
 
         val body = RequestBody.create(MediaType.parse("application/json"), jsonStr)
+        Logger.log(VerboseLevel.INFO, "$fn: jsonStr=\n$jsonStr")
 
         Observable.fromCallable {
             service.run {
                 when(msgType){
-                    SEND_GPS_SAMPLE, SEND_CAM_SAMPLE, SEND_MIC_SAMPLE ->putSample(expId, body)
+                    SEND_GPS_SAMPLE, SEND_CAM_SAMPLE, SEND_MIC_SAMPLE ->putSample(expId, jsonStr)
                     //SOME OTHER MSG TYPES HERE -> DO STUFF
                 }
             }
@@ -67,4 +74,27 @@ object RemoteDbHandler
                 .subscribeOn(Schedulers.io())
                 .subscribe()
     }
+
+    fun getAllExp() {
+        val fn = Throwable().stackTrace[0].methodName
+        Logger.log(VerboseLevel.INFO, "$fn: called.")
+        var list: List<Experiment>
+        Observable.fromCallable {
+            service.getAllExperiments()
+        }
+                .doOnNext{
+                    it.enqueue(object : Callback<JsonElement> {
+                        override fun onResponse(call: Call<JsonElement>, response: Response<JsonElement>) {
+                            Logger.log(VerboseLevel.INFO, "$fn: got all experiments.")
+                            Logger.log(VerboseLevel.INFO, "$fn: body = \n${response.body().toString()}")
+                        }
+
+                        override fun onFailure(call: Call<JsonElement>, t: Throwable) {
+                            Logger.log(VerboseLevel.INFO, "$fn: failed to get all experiments.")
+                        }
+                    })
+                }
+                .subscribeOn(Schedulers.io())
+                .subscribe()
+        }
 }
