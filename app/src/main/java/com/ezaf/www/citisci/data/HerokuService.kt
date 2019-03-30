950930038cecc9dd63fc7f7d005478971b1d3e54
ShayRubach <shayrubach@gmail.com>
2019-03-30 12:14:09 +0200

    HerokuService: fixed wrong types and urls of REST

--- app/src/main/java/com/ezaf/www/citisci/data/HerokuService.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/HerokuService.kt
@@ -1,8 +1,5 @@
 package com.ezaf.www.citisci.data
-import com.google.gson.Gson
 import com.google.gson.JsonElement
-import okhttp3.RequestBody
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.*
 
@@ -11,10 +8,10 @@ interface HerokuService {
     @GET("https://tempcitisci.herokuapp.com/api/v1/experiments")
     fun getAllExperiments(): Call<JsonElement>
 
-    @GET("hhttps://tempcitisci.herokuapp.com/api/v1/experiments/{id}")
-    fun getExpById(@Path("id")id: String): Call<ResponseBody>
+    @GET("https://tempcitisci.herokuapp.com/api/v1/experiments/{id}")
+    fun getExpById(@Path("id")id: String): Call<JsonElement>
 
-    @PUT("https://tempcitisci.herokuapp.com/api/v1/experiments/{id}/samples/")
-    fun putSample(@Path("id") id: String, @Body body: String): Call<ResponseBody>
+    @PUT("https://tempcitisci.herokuapp.com/api/v1/experiments/{id}/samples")
+    fun putSample(@Path("id") id: String, @Body body: ExpSample): Call<ExpSample>
 
 }
