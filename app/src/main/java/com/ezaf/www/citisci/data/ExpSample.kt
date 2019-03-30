370486ef6bfca06035f81b18fa4144ded5ed8f32
ShayRubach <shayrubach@gmail.com>
2019-03-30 12:13:37 +0200

    ExpSample: removed redundant code. refactored some

--- app/src/main/java/com/ezaf/www/citisci/data/ExpSample.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/ExpSample.kt
@@ -1,14 +1,8 @@
 package com.ezaf.www.citisci.data
 
-abstract class ExpSample(var sensorType: SensorType){
-    val sender: String = "SOME_MAIL@SOME_DOMAIN.COM"
+class ExpSample(val actionID: String,
+                val samples: List<Any>){
+    val email: String = "SOME_MAIL@SOME_DOMAIN.COM"
 }
 
-class ExpGpsSample(val data: LatLong) : ExpSample(SensorType.GPS)
-class ExpCameraSample(val data: String) : ExpSample(SensorType.Camera)
-class ExpMicSample(val data: String) : ExpSample(SensorType.Michrophone)
-
-class LatLong(val latitude: String, val longitude: String){
-
-    constructor(lat: Double, lon: Double) : this(lat.toString(), lon.toString())
-}
+class LatLong(val latitude: Double, val longitude: Double){}
