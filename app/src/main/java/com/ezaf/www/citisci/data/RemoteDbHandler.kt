5db92f550d23483f2e58578a058ca945fd6e8099
ShayRubach <shayrubach@gmail.com>
2019-03-31 11:49:33 +0200

    RemoteDbHandler: calling parser to handle the complex json of experiments and return a list of them

--- app/src/main/java/com/ezaf/www/citisci/data/RemoteDbHandler.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/RemoteDbHandler.kt
@@ -8,9 +8,27 @@ import retrofit2.Callback
 import retrofit2.Response
 import com.ezaf.www.citisci.data.RemoteDbHandler.MsgType.*
 import com.ezaf.www.citisci.utils.Logger
+import com.ezaf.www.citisci.utils.TypeConverterUtil
 import com.ezaf.www.citisci.utils.VerboseLevel
+import com.google.gson.Gson
 import com.google.gson.JsonElement
+import com.google.gson.reflect.TypeToken
 import retrofit2.converter.gson.GsonConverterFactory
+import com.google.gson.JsonObject
+import android.R.id.message
+import org.json.JSONObject
+import org.json.JSONArray
+import android.R.attr.name
+import android.R.id
+import com.ezaf.www.citisci.utils.ParserUtil
+import java.lang.reflect.Type;
+
+
+
+
+
+
+
 
 object RemoteDbHandler
 {
@@ -63,26 +81,28 @@ object RemoteDbHandler
                 .subscribe()
     }
 
-    fun getAllExp() {
+    fun getAllExp() : List<Experiment> {
         val fn = Throwable().stackTrace[0].methodName
         Logger.log(VerboseLevel.INFO, "$fn: called.")
-        var list: List<Experiment>
+        var list: MutableList<Experiment> = mutableListOf()
         Observable.fromCallable {
             service.getAllExperiments()
-        }
-                .doOnNext{
-                    it.enqueue(object : Callback<JsonElement> {
-                        override fun onResponse(call: Call<JsonElement>, response: Response<JsonElement>) {
-                            Logger.log(VerboseLevel.INFO, "$fn: got all experiments.")
-                            Logger.log(VerboseLevel.INFO, "$fn: body = \n${response.body().toString()}")
-                        }
-
-                        override fun onFailure(call: Call<JsonElement>, t: Throwable) {
-                            Logger.log(VerboseLevel.INFO, "$fn: failed to get all experiments.")
-                        }
-                    })
+        }.doOnNext{
+            it.enqueue(object : Callback<JsonElement> {
+                override fun onResponse(call: Call<JsonElement>, response: Response<JsonElement>) {
+                    Logger.log(VerboseLevel.INFO, "$fn: got all experiments.")
+                    list = ParserUtil.jsonToExpList(response.body().toString())
                 }
+
+
+
+                override fun onFailure(call: Call<JsonElement>, t: Throwable) {
+                    Logger.log(VerboseLevel.INFO, "$fn: failed to get all experiments.")
+                }
+            })
+        }
                 .subscribeOn(Schedulers.io())
                 .subscribe()
-        }
+        return list
+    }
 }
