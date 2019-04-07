02b7a411c4fef2232996b130cea991ee21341f4b
ShayRubach <shayrubach@gmail.com>
2019-04-07 12:47:17 +0200

    MyExperiment/Adapter/layout: now displaying participating experiments on MyExperiments fragment

--- app/src/main/java/com/ezaf/www/citisci/ui/MyExperiments.kt
+++ app/src/main/java/com/ezaf/www/citisci/ui/MyExperiments.kt
@@ -1,33 +1,74 @@
 package com.ezaf.www.citisci.ui
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ezaf.www.citisci.R
+import com.ezaf.www.citisci.data.exp.Experiment
+import com.ezaf.www.citisci.data.exp.SharedDataHelper
+import com.ezaf.www.citisci.utils.Logger
+import com.ezaf.www.citisci.utils.VerboseLevel
+import com.ezaf.www.citisci.utils.adapter.MyExperimentsAdapter
 import com.ezaf.www.citisci.utils.viewmodel.MyExperimentsViewModel
-import kotlinx.android.synthetic.main.my_experiments_fragment.*
-import java.util.*
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 
-class MyExperiments : Fragment() {
+class MyExperiments : FeedPage() {
 
     companion object {
         fun newInstance() = MyExperiments()
     }
 
     private lateinit var viewModel: MyExperimentsViewModel
+    override lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.my_experiments_fragment, container, false)
+        val rootView = inflater.inflate(R.layout.my_experiments_fragment, container, false)
+        setupRecycler(rootView)
+        return rootView
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    @SuppressLint("CheckResult")
+    override fun setupRecycler(rootView: View) {
+        var fn = Throwable().stackTrace[0].methodName
+
+        Observable.fromCallable {
+            MainActivity.localDbHandler.experimentDao().getMyExp()
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Logger.log(VerboseLevel.INFO, "$fn: list size = ${it.size}")
+                    recyclerView = rootView.findViewById(R.id.MyExperimentsRecyclerView)
+                    recyclerView.run {
+                        layoutManager = LinearLayoutManager(context)
+
+                        //set click listener for item clicked in list
+                        val itemOnClick: (Int, Experiment) -> Unit = { position, exp ->
+                            //                            this.adapter!!.notifyDataSetChanged()
+                            SharedDataHelper.focusedExp = exp
+                            val nextAction = FeedPageDirections.nextAction()
+                            Navigation.findNavController(rootView).navigate(nextAction)
+                            Logger.log(VerboseLevel.INFO, "$fn: called.\n clicked item no. $position")
+
+                        }
+
+                        adapter = MyExperimentsAdapter(it, context, itemOnClick)
+                        addItemDecoration(DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL))
+                        runLayoutAnimation(this)
+                    }
+                }
+        //TODO: dispose
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/ezaf/www/citisci/utils/adapter/MyExperimentsAdapter.kt
@@ -0,0 +1,74 @@
+package com.ezaf.www.citisci.utils.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ezaf.www.citisci.R
+import com.ezaf.www.citisci.data.exp.Experiment
+import com.ezaf.www.citisci.data.exp.SAMPLES_ACQUIRED_PREFIX
+import com.ezaf.www.citisci.utils.Logger
+import com.ezaf.www.citisci.utils.VerboseLevel
+import kotlinx.android.synthetic.main.my_experiment_row.view.*
+
+class MyExperimentsAdapter(private val items: List<Experiment>, val context: Context, private val itemClickListener: (Int, Experiment) -> Unit)
+    : RecyclerView.Adapter<MyExperimentViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyExperimentViewHolder {
+        return MyExperimentViewHolder(LayoutInflater.from(context).inflate(R.layout.my_experiment_row, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MyExperimentViewHolder, position: Int) {
+        val exp: Experiment = items[position]
+        holder.bind(exp, itemClickListener)
+    }
+}
+
+class MyExperimentViewHolder (view: View) : FeedPageViewHolder(view) {
+
+    private val mSamplesAcquired = view.myExp_samplesAcquired
+    private val mProgressRect = view.myExp_progressRect
+    override val mView = view
+    override val mName = view.myExp_expName
+    override val mResearcher = view.myExp_previewResearcherName
+    override val mProgress = view.myExp_expProgress
+    override val mType = view.myExp_expType
+    override val mSensors = listOf(
+            view.myExp_sensorType1,
+            view.myExp_sensorType2,
+            view.myExp_sensorType3,
+            view.myExp_sensorType4)
+
+
+    override fun bind(exp: Experiment, itemClickListener: (Int, Experiment) -> Unit) {
+
+        val samplesStatus = exp.getSamplesForDisplay()
+        var percentageCompleted = 0
+
+        if(!exp.basicData.automatic){
+            percentageCompleted = samplesStatus.first / samplesStatus.second
+        }
+
+        exp.basicData.run {
+            mName.text = name
+            mResearcher.text = "Researcher Name"
+            mProgress.text = percentageCompleted.toString()+"%"
+            mSamplesAcquired.text = toFixedSamplesAcquiredDisplay(samplesStatus)
+            setExpTypeImageResource(mType, automatic)
+        }
+
+        mProgressRect.width = mView.width * percentageCompleted
+        setSensorImageResouce(mSensors, exp.getUniqueParticipatingSensorType())
+        mView.setOnClickListener { itemClickListener(adapterPosition, exp) }
+    }
+
+    private fun toFixedSamplesAcquiredDisplay(samplesForDisplay: Pair<Int, Int>): String {
+        return SAMPLES_ACQUIRED_PREFIX + samplesForDisplay.first + "/" + samplesForDisplay.second
+    }
+
+}
