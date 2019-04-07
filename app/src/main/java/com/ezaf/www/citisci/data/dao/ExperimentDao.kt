5bb41e976d7724368be1115dddee2903ad74af94
ShayRubach <shayrubach@gmail.com>
2019-04-07 12:53:26 +0200

    ExperimentDao: added some usefull queries

--- app/src/main/java/com/ezaf/www/citisci/data/dao/ExperimentDao.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/dao/ExperimentDao.kt
@@ -6,12 +6,16 @@ import com.ezaf.www.citisci.data.exp.Experiment
 const val Q_GET_EXP_BY_ID   = "SELECT * FROM Experiment WHERE _id == :id"
 const val Q_GET_ALL_EXP     = "SELECT * FROM Experiment"
 const val Q_UPDATE_PARTICIPATING_STATUS = "UPDATE Experiment SET participating = '1' WHERE _id == :id"
-
+const val Q_GET_MY_EXP     = "SELECT * FROM Experiment WHERE participating='1'"
+const val Q_GET_ALL_OTHER_EXP = "SELECT * FROM Experiment WHERE participating !='1'"
 
 @Dao
 interface ExperimentDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertExp(exp: Experiment)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertExpList(list: List<Experiment>)
 
     @Update
     fun updateExp(exp: Experiment)
@@ -25,7 +29,14 @@ interface ExperimentDao {
     @Query(Q_GET_ALL_EXP)
     fun getAllExp(): List<Experiment>
 
+    @Query(Q_GET_ALL_OTHER_EXP)
+    fun getAllOtherExp(): List<Experiment>
+
+
     @Query(Q_UPDATE_PARTICIPATING_STATUS)
     fun joinExp(id: String)
+
+    @Query(Q_GET_MY_EXP)
+    fun getMyExp(): List<Experiment>
 
 }
