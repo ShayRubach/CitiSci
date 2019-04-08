54a35c4e580beccc0a47fe34262f2f8805a5816a
ShayRubach <shayrubach@gmail.com>
2019-04-08 14:16:28 +0200

    MainActivity: get experiments at app startup

--- app/src/main/java/com/ezaf/www/citisci/ui/MainActivity.kt
+++ app/src/main/java/com/ezaf/www/citisci/ui/MainActivity.kt
@@ -25,11 +25,21 @@ import com.ezaf.www.citisci.data.exp.ExpAction
 import com.ezaf.www.citisci.data.exp.ExpBasicData
 import com.ezaf.www.citisci.data.exp.Experiment
 import com.ezaf.www.citisci.utils.Interpreter
+import com.ezaf.www.citisci.utils.Logger
+import com.ezaf.www.citisci.utils.Logger.log
+import com.ezaf.www.citisci.utils.ParserUtil
+import com.ezaf.www.citisci.utils.VerboseLevel
 import com.ezaf.www.citisci.utils.db.LocalDbHandler
 import com.ezaf.www.citisci.utils.service.LocationUpdateService
 import com.ezaf.www.citisci.utils.db.RemoteDbHandler
+import com.google.gson.JsonElement
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.time.Instant
 import java.util.*
 import kotlin.concurrent.schedule
@@ -66,38 +76,24 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
-
-
-
-
-
-
-
-
 //        goToCameraBtn.setOnClickListener{
 //            startActivity(Intent(this, CameraActivity::class.java))
 //            finish()
 //        }
 
-//        goToGpsActivity.setOnClickListener {
-////            var interpreter = Interpreter
-////            interpreter.stopAllScripts()
-//            startActivity(Intent(this, GpsLocationActivity::class.java))
-//            finish()
-//        }
 
         if(checkPermissions()){
             requestPermissions()
         }
-        //will be called on the permission result handler
+
+        //called on the permission result handler
 //        startForegroundService(Intent(this, LocationUpdateService::class.java))
 
         localDbHandler = LocalDbHandler.getLocalDb(context = this)
         expDao = localDbHandler.experimentDao()
         expActionDao = localDbHandler.expActionsDao()
-        testDbInsertionAndSelection()
+//        testDbInsertionAndSelection()
+
         getAllExpFromRemoteDb()
 
 
@@ -137,27 +133,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getAllExpFromRemoteDb() = runBlocking {
-        var interpreter = Interpreter
-
-        async {
-            list  = RemoteDbHandler.getAllExp().toMutableList()
-        }
-
-        //TODO: REMOVE THIS DELAY AND MANAGE CALLS WITH task.await() with a manager to invoke them by seq!!!!
-//        Timer("SettingUp", false).schedule(3000) {
-//            for(ex in list){
-//                interpreter.playScripts(ex._id)
-//            }
-//        }
-
-//        val task2 = async {
-//            log(INFO_ERR, " \n################## IM DONE #################################### IM DONE ##################\n")
-//        }
 
 
-//        task1.await()
-//        task2.await()
+        RemoteDbHandler.getAllExp()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    it.enqueue(object : Callback<JsonElement> {
+                        override fun onResponse(call: Call<JsonElement>, response: Response<JsonElement>) {
+                            ParserUtil.jsonToExpList(response.body().toString(), list)
+                            Logger.log(VerboseLevel.INFO, "got all experiments.")
+                            localDbHandler.insertExpList(list)
+                        }
 
+                        override fun onFailure(call: Call<JsonElement>, t: Throwable) {
+                            Logger.log(VerboseLevel.INFO, "failed to get all experiments.")
+                        }
+                    })
+
+
+                }
     }
 
     fun testDbInsertionAndSelection(){
@@ -166,25 +161,24 @@ class MainActivity : AppCompatActivity() {
         val desc1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         val desc2 = "Risus in hendrerit gravida rutrum quisque non tellus orci. Laoreet sit amet cursus sit amet dictum. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Neque vitae tempus quam pellentesque nec nam aliquam sem. Urna nec tincidunt praesent semper feugiat nibh. Blandit cursus risus at ultrices. Sed lectus vestibulum mattis ullamcorper velit. Varius duis at consectetur lorem donec massa sapien faucibus. Viverra vitae congue eu consequat ac felis. Faucibus nisl tincidunt eget nullam non nisi est. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Purus sit amet volutpat consequat. Malesuada proin libero nunc consequat interdum. Sed cras ornare arcu dui. Morbi blandit cursus risus at ultrices. A iaculis at erat pellentesque adipiscing commodo elit. Pulvinar elementum integer enim neque. Risus feugiat in ante metus dictum at tempor. Risus pretium quam vulputate dignissim suspendisse in."
 
+        var bdata = ExpBasicData("1","Morning Glory", Instant.now(),false,desc1,"guide")
+        var bdata2 = ExpBasicData("123","How far?", Instant.now(),true,desc2,"gg")
 
         Observable.fromCallable {
             localDbHandler.clearAllTables()
 
         }.doOnNext {
-            var bdata = ExpBasicData("1","Morning Glory", Instant.now(),false,desc1,"guide")
-            var bdata2 = ExpBasicData("123","How far?", Instant.now(),true,desc2,"gg")
+//            var action1 = ExpAction(1.0,60,500,"ACTION_1", SensorType.GPS, listOf("32.060709$34.812342$1000.0"))
+//            var action2 = ExpAction(2.0,60,10,"ACTION_2", SensorType.Camera, listOf("32.060709$34.812342$1000.0"))
+//            var action3 = ExpAction(3.0,10,1,"ACTION_3", SensorType.Michrophone, listOf("32.060709$34.812342$1000.0"))
+//            var action4 = ExpAction(2.22,100,32,"ACTION_4", SensorType.Michrophone, listOf("1.3$7.5$23"))
+//            var action5 = ExpAction(2.4,30,402,"ACTION_5", SensorType.Camera, listOf("3.3.0$234.5$722"))
+//
+//            var exp1 = Experiment("23sdf78sd",bdata, mutableListOf(action1._id,action2._id,action3._id))
+//            var exp2 = Experiment("9sdf87122",bdata2, mutableListOf(action4._id,action5._id))
 
-            var action1 = ExpAction(1.0,60,500,"ACTION_1", SensorType.GPS, listOf("32.060709$34.812342$1000.0"))
-            var action2 = ExpAction(2.0,60,10,"ACTION_2", SensorType.Camera, listOf("32.060709$34.812342$1000.0"))
-            var action3 = ExpAction(3.0,1,1,"ACTION_3", SensorType.Michrophone, listOf("32.060709$34.812342$1000.0"))
-            var action4 = ExpAction(2.22,1,32,"ACTION_4", SensorType.Michrophone, listOf("1.3$7.5$23"))
-            var action5 = ExpAction(2.4,3,402,"ACTION_5", SensorType.Camera, listOf("3.3.0$234.5$722"))
-
-            var exp1 = Experiment("23sdf78sd",bdata, mutableListOf(action1._id,action2._id,action3._id))
-            var exp2 = Experiment("9sdf87122",bdata2, mutableListOf(action4._id,action5._id))
-
-            MainActivity.list.add(exp1)
-            MainActivity.list.add(exp2)
+//            MainActivity.list.add(exp1)
+//            MainActivity.list.add(exp2)
 
 
         }
