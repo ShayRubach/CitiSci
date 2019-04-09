5917cb79637d36365e9fc8f9519b16f9007173b5
ShayRubach <shayrubach@gmail.com>
2019-04-09 04:51:49 +0200

    SharedDataHelper: added new local list for user only experiments

--- app/src/main/java/com/ezaf/www/citisci/data/exp/SharedDataHelper.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/exp/SharedDataHelper.kt
@@ -3,4 +3,5 @@ package com.ezaf.www.citisci.data.exp
 object SharedDataHelper {
     lateinit var focusedExp: Experiment
     var listOfAllExp : MutableList<Experiment> = mutableListOf()
+    var listOfMyExp : MutableList<Experiment> = mutableListOf()
 }
