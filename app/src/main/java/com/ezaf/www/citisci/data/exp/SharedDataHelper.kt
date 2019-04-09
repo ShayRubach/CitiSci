1eb29d15b42df650620276654b60a93f659a65d8
ShayRubach <shayrubach@gmail.com>
2019-04-09 04:14:53 +0200

    SharedDataHelper: renamed global list

--- app/src/main/java/com/ezaf/www/citisci/data/exp/SharedDataHelper.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/exp/SharedDataHelper.kt
@@ -2,5 +2,5 @@ package com.ezaf.www.citisci.data.exp
 
 object SharedDataHelper {
     lateinit var focusedExp: Experiment
-    var list : MutableList<Experiment> = mutableListOf()
+    var listOfAllExp : MutableList<Experiment> = mutableListOf()
 }
