4cfea8218732955d45881f398cde4a937d44b131
ShayRubach <shayrubach@gmail.com>
2019-04-09 04:52:53 +0200

    HerokuService: added new apis. changed PUT to POST

--- app/src/main/java/com/ezaf/www/citisci/utils/service/HerokuService.kt
+++ app/src/main/java/com/ezaf/www/citisci/utils/service/HerokuService.kt
@@ -1,7 +1,7 @@
 package com.ezaf.www.citisci.utils.service
 import com.ezaf.www.citisci.data.exp.ExpSample
 import com.google.gson.JsonElement
-import retrofit2.Call;
+import retrofit2.Call
 import retrofit2.http.*
 
 interface HerokuService {
@@ -9,12 +9,19 @@ interface HerokuService {
     @GET("https://tempcitisci.herokuapp.com/api/v1/experiments")
     fun getAllExperiments(): Call<JsonElement>
 
+    @GET("https://tempcitisci.herokuapp.com/api/v1/users/{email}/experiments")
+    fun getMyExperiments(@Path("email")email: String): Call<JsonElement>
+
     @GET("https://tempcitisci.herokuapp.com/api/v1/experiments/{id}")
     fun getExpById(@Path("id")id: String): Call<JsonElement>
 
     @PUT("https://tempcitisci.herokuapp.com/api/v1/experiments/{id}/samples")
     fun putSample(@Path("id") id: String, @Body body: ExpSample): Call<ExpSample>
 
-    @PUT("https://tempcitisci.herokuapp.com/api/v1/experiments/{id}/samples")
+    @POST("https://tempcitisci.herokuapp.com/api/v1/experiments/{id}/samples")
     fun putSampleList(@Path("id") id: String, @Body body: List<ExpSample>): Call<List<ExpSample>>
+
+    @PUT("https://tempcitisci.herokuapp.com/api/v1/experiments/{expId}/participants/{email}")
+    fun joinExp(expId: String, email: String): Call<JsonElement>
+
 }
