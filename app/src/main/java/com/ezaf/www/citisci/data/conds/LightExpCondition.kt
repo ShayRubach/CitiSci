df8efa34147d72a1e6839d15c9ceaa00a61239aa
ShayRubach <shayrubach@gmail.com>
2019-04-19 03:19:49 +0200

    TimeExpCondition, LightExpCondition: added new condition be considered while running the script

---
+++ app/src/main/java/com/ezaf/www/citisci/data/conds/LightExpCondition.kt
@@ -0,0 +1,12 @@
+package com.ezaf.www.citisci.data.conds
+
+import com.ezaf.www.citisci.data.exp.ExpCondition
+import com.ezaf.www.citisci.data.exp.SharedDataHelper
+import com.ezaf.www.citisci.utils.service.LightMode
+
+class LightExpCondition(private val mode: LightMode) : ExpCondition {
+
+    override fun isConditionMet(): Boolean {
+        return mode == SharedDataHelper.lightMode
+    }
+}
---
+++ app/src/main/java/com/ezaf/www/citisci/data/conds/TimeExpCondition.kt
@@ -0,0 +1,17 @@
+package com.ezaf.www.citisci.data.conds
+
+import com.ezaf.www.citisci.data.exp.ExpBasicData.Companion.toInstant
+import com.ezaf.www.citisci.data.exp.ExpCondition
+import java.time.Instant
+
+class TimeExpCondition (private val startTime: String, private val endTime: String) : ExpCondition {
+
+    override fun isConditionMet(): Boolean {
+        val startTimeInstant = toInstant(startTime)
+        val endTimeInstant = toInstant(endTime)
+        val nowTime = Instant.now()
+
+        return (nowTime.isBefore(startTimeInstant) && nowTime.isAfter(endTimeInstant))
+    }
+
+}
