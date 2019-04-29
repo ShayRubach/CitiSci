c8cf3244f3550a7be28a1523ca18f5ba66e00c9b
ShayRubach <shayrubach@gmail.com>
2019-04-29 12:17:29 +0200

    GpsExpCondition: removed old unused code

--- app/src/main/java/com/ezaf/www/citisci/data/conds/GpsExpCondition.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/conds/GpsExpCondition.kt
@@ -35,25 +35,6 @@ class GpsExpCondition(private val baseCoord: Pair<Double,Double>,
         return loc1.distanceTo(loc2).toDouble()
     }
 
-    companion object {
-        fun toExpConditionList(actionList: List<String>) : List<GpsExpCondition> {
-            val fn = Throwable().stackTrace[0].methodName
-
-            val delim = "$"
-            var splittedStr: List<String>
-            var newList: MutableList<GpsExpCondition> = mutableListOf()
-
-            for(str in actionList){
-                splittedStr = str.split(delim)
-                newList.add(GpsExpCondition(Pair(splittedStr[0].toDouble(), splittedStr[1].toDouble()), splittedStr[2].toDouble()))
-            }
-            if(newList.isEmpty()){
-                log(VerboseLevel.ERR,"$fn: empty list was generated")
-            }
-            return newList
-        }
-    }
-
     override fun toString(): String {
         return "latitude= ${baseCoord.first}, longitude = ${baseCoord.second}, maxRadios = $maxRadius\n"
     }
