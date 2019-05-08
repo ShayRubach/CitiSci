ebd980807044e507dfb72cc0af7d12f50a3518f6
ShayRubach <shayrubach@gmail.com>
2019-05-08 08:59:17 +0200

    TimeExpCondition: fixed time formatting. now working.

--- app/src/main/java/com/ezaf/www/citisci/data/conds/TimeExpCondition.kt
+++ app/src/main/java/com/ezaf/www/citisci/data/conds/TimeExpCondition.kt
@@ -2,9 +2,18 @@ package com.ezaf.www.citisci.data.conds
 
 import com.ezaf.www.citisci.data.exp.ExpBasicData.Companion.toInstant
 import com.ezaf.www.citisci.data.exp.ExpCondition
+import com.ezaf.www.citisci.utils.Logger
+import com.ezaf.www.citisci.utils.Logger.log
+import com.ezaf.www.citisci.utils.VerboseLevel
 import java.time.Instant
+import java.time.LocalTime
 
 class TimeExpCondition (private val after: String, private val before: String) : ExpCondition {
+
+    private val delim = ":"
+    private val HOURS_IDX = 0
+    private val MINUTES_IDX = 1
+
 
     override fun isConditionMet(): Boolean {
 
@@ -12,11 +21,16 @@ class TimeExpCondition (private val after: String, private val before: String) :
             return false
         }
 
-        val startTimeInstant = toInstant(after)
-        val endTimeInstant = toInstant(before)
-        val nowTime = Instant.now()
+        val afterTime = LocalTime.of(after.substring(0,2).toInt(), after.substring(2).toInt())
+        val beforeTime = LocalTime.of(before.substring(0,2).toInt(), before.substring(2).toInt())
+        val nowTime = LocalTime.now()
 
-        return (nowTime.isBefore(startTimeInstant) && nowTime.isAfter(endTimeInstant))
+        Logger.log(VerboseLevel.LOCATION, "beforeTime=$beforeTime")
+        Logger.log(VerboseLevel.LOCATION, "afterTime=$afterTime")
+        Logger.log(VerboseLevel.LOCATION, "nowTime=$nowTime")
+
+
+        return (nowTime.isBefore(beforeTime) && nowTime.isAfter(afterTime))
     }
 
     override fun toString(): String {
