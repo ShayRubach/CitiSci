ac39b265136eef90e7ab9b4d10425a2f93eba772
ShayRubach <shayrubach@gmail.com>
2019-05-18 05:45:24 +0200

    ManualCapturing.kt: moved logic to viewModel

--- app/src/main/java/com/ezaf/www/citisci/ui/ManualCapturing.kt
+++ app/src/main/java/com/ezaf/www/citisci/ui/ManualCapturing.kt
@@ -65,44 +65,20 @@ class ManualCapturing : Fragment() {
     }
 
     private fun setSensorCurrentValues(sensorType: SensorType) {
-        tvCurrentSensorValue.run {
-            text = when (sensorType){
-                SensorType.GPS-> getFormattedLocationStr()
-                SensorType.MAGNETIC_FIELD -> getFormattedMagFieldsStr()
+        tvCurrentSensorValue.text = when (sensorType){
+                SensorType.GPS-> viewModel.getFormattedLocationStr()
+                SensorType.MAGNETIC_FIELD -> viewModel.getFormattedMagFieldsStr()
                 else -> "wtf?"
-            }
         }
-    }
-
-    private fun getFormattedLocationStr(): String {
-        return LocationUpdateService.lastLocationCaptured.latitude.toString() + "\n" +LocationUpdateService.lastLocationCaptured.longitude.toString()
-    }
-
-    private fun getFormattedMagFieldsStr(): String {
-        return  "X: " + SharedDataHelper.magneticFieldValues[0].toString() + "\n" +
-                "Y: " + SharedDataHelper.magneticFieldValues[1].toString() + "\n" +
-                "Z: " + SharedDataHelper.magneticFieldValues[2].toString() + "\n"
     }
 
     private fun setSensorIdentifierMsg(sensorType: SensorType ) {
-        tvCurrentSensorIdentifierMsg.run {
-            text = when (sensorType){
-                SensorType.GPS-> getString(R.string.identifier_msg_gps)
-                SensorType.MAGNETIC_FIELD -> getString(R.string.identifier_msg_magnetic)
-                else -> "wtf?"
-            }
-        }
-
+        tvCurrentSensorIdentifierMsg.text = getString(viewModel.getSensorIdentifierMsgId(sensorType))
     }
 
     private fun setSensorImage(sensorType: SensorType ) {
         ivSensorIcon.run {
-            //set sensor image
-            when (sensorType){
-                SensorType.GPS-> setImageResource(R.drawable.ic_sensor_gps)
-                SensorType.MAGNETIC_FIELD -> setImageResource(R.drawable.ic_sensor_magnetic_field)
-                else -> setImageResource(R.drawable.ic_citisci_logo)
-            }
+            setImageResource(viewModel.getSensorImage(sensorType))
             layoutParams.height = 300 //TODO: fetch real imageview size here
             layoutParams.width = 300
 
@@ -120,8 +96,8 @@ class ManualCapturing : Fragment() {
         val fn = Throwable().stackTrace[0].methodName
 
         btnManualCapture.setOnClickListener {
-            val msgType = getMsgType()
-            val sampleList = prepareSampleList()
+            val msgType = viewModel.getMsgType(sensorType)
+            val sampleList = viewModel.prepareSampleList(exp)
 
             RemoteDbHandler.sendMsg(msgType, sampleList).
                     doOnNext{
@@ -140,43 +116,6 @@ class ManualCapturing : Fragment() {
                     }
                     .subscribeOn(Schedulers.io())
                     .subscribe()
-        }
-    }
-
-    private fun prepareSampleList(): ExpSampleList {
-        val fn = Throwable().stackTrace[0].methodName
-
-        val sampleList = ExpSampleList()
-        lateinit var sample : ExpSample
-        val thisAction = exp.actions[0]
-        when(sensorType){
-            SensorType.GPS -> {
-                val location = DataCollector.collect(sensorType) as Location
-                sample = ExpSample(thisAction.expId, thisAction._id, "participant@gmail.com", LatLong(location.latitude, location.longitude))
-            }
-
-            SensorType.MAGNETIC_FIELD -> {
-                sample = ExpSample(thisAction.expId, thisAction._id, "participant@gmail.com",
-                        MagneticFields(
-                                magneticFieldValues[0],
-                                magneticFieldValues[1],
-                                magneticFieldValues[2])
-                )
-
-            }
-            else -> Logger.log(VerboseLevel.INFO, "$fn: failed to prepare sample. action=$thisAction.")
-        }
-
-        sampleList.addSample(sample)
-
-        return sampleList
-    }
-
-    private fun getMsgType(): RemoteDbHandler.MsgType  {
-        return when(sensorType){
-            SensorType.GPS -> RemoteDbHandler.MsgType.SEND_GPS_SAMPLE
-            SensorType.MAGNETIC_FIELD ->RemoteDbHandler.MsgType.SEND_MAGNETIC_FIELD_SAMPLE
-            else -> RemoteDbHandler.MsgType.SEND_MIC_SAMPLE
         }
     }
 
--- app/src/main/java/com/ezaf/www/citisci/utils/viewmodel/ManualCapturingViewModel.kt
+++ app/src/main/java/com/ezaf/www/citisci/utils/viewmodel/ManualCapturingViewModel.kt
@@ -1,7 +1,81 @@
 package com.ezaf.www.citisci.utils.viewmodel
 
+import android.location.Location
 import androidx.lifecycle.ViewModel
+import com.ezaf.www.citisci.R
+import com.ezaf.www.citisci.data.DataCollector
+import com.ezaf.www.citisci.data.SensorType
+import com.ezaf.www.citisci.data.exp.*
+import com.ezaf.www.citisci.utils.Logger
+import com.ezaf.www.citisci.utils.VerboseLevel
+import com.ezaf.www.citisci.utils.db.RemoteDbHandler
+import com.ezaf.www.citisci.utils.service.LocationUpdateService
 
 class ManualCapturingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun getSensorImage(sensorType: SensorType): Int {
+        return when (sensorType){
+            SensorType.GPS-> R.drawable.ic_sensor_gps
+            SensorType.MAGNETIC_FIELD -> R.drawable.ic_sensor_magnetic_field
+            else -> R.drawable.ic_citisci_logo
+        }
+    }
+
+    fun getSensorIdentifierMsgId(sensorType: SensorType): Int {
+        return when (sensorType){
+            SensorType.GPS-> R.string.identifier_msg_gps
+            SensorType.MAGNETIC_FIELD -> R.string.identifier_msg_magnetic
+            else -> R.string.error
+
+        }
+    }
+
+    fun getMsgType(sensorType: SensorType): RemoteDbHandler.MsgType {
+        return when(sensorType){
+            SensorType.GPS -> RemoteDbHandler.MsgType.SEND_GPS_SAMPLE
+            SensorType.MAGNETIC_FIELD ->RemoteDbHandler.MsgType.SEND_MAGNETIC_FIELD_SAMPLE
+            else -> RemoteDbHandler.MsgType.SEND_MIC_SAMPLE
+        }
+    }
+
+    fun prepareSampleList(exp: Experiment): ExpSampleList {
+        val fn = Throwable().stackTrace[0].methodName
+        val thisAction = exp.actions[0]
+        val sensorType = thisAction.sensorType
+        lateinit var sample : ExpSample
+        val sampleList = ExpSampleList()
+
+        when(sensorType){
+            SensorType.GPS -> {
+                val location = DataCollector.collect(sensorType) as Location
+                sample = ExpSample(thisAction.expId, thisAction._id, "participant@gmail.com", LatLong(location.latitude, location.longitude))
+            }
+
+            SensorType.MAGNETIC_FIELD -> {
+                sample = ExpSample(thisAction.expId, thisAction._id, "participant@gmail.com",
+                        MagneticFields(
+                                SharedDataHelper.magneticFieldValues[0],
+                                SharedDataHelper.magneticFieldValues[1],
+                                SharedDataHelper.magneticFieldValues[2])
+                )
+
+            }
+            else -> Logger.log(VerboseLevel.INFO, "$fn: failed to prepare sample. action=$thisAction.")
+        }
+
+        sampleList.addSample(sample)
+
+        return sampleList
+    }
+
+    fun getFormattedLocationStr(): String {
+        return LocationUpdateService.lastLocationCaptured.latitude.toString() + "\n" + LocationUpdateService.lastLocationCaptured.longitude.toString()
+    }
+
+    fun getFormattedMagFieldsStr(): String {
+        return  "X: " + SharedDataHelper.magneticFieldValues[0].toString() + "\n" +
+                "Y: " + SharedDataHelper.magneticFieldValues[1].toString() + "\n" +
+                "Z: " + SharedDataHelper.magneticFieldValues[2].toString() + "\n"
+    }
+
 }
